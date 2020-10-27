27ccbf3cc771520ebf195044035e964a0d4b5ee6
absolutelyfullycapable <lovinsurfaces@gmail.com>
2020-10-27 22:00:59 +0900

    modify files - change file name + about og url link

---
+++ js/about.js
@@ -0,0 +1,16 @@
+$(function() {
+    // 빈 링크 가진 a 기본 성격 제거
+    $('a[href="#"]').on('click', function(e) {
+        e.preventDefault();
+    });
+
+    // header 안 .mobile_btn 누르면 nav 나타남
+    $('header .mobile_btn').on('click', function() {
+        $('nav').addClass('show');
+    });
+
+    // nav 안 X 버튼 누르면 nav 다시 사라짐
+    $('nav div img').on('click', function() {
+        $('nav').removeClass('show');
+    });
+});
