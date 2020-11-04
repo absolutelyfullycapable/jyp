f0f34ac6e9124a906e3f91d8010abb50362f85a8
absolutelyfullycapable <lovinsurfaces@gmail.com>
2020-11-04 17:49:28 +0900

    modify index HTML, CSS, JS/jQuery - artist section

--- js/index.js
+++ js/index.js
@@ -13,4 +13,15 @@ $(function() {
     $('nav div img').on('click', function() {
         $('nav').removeClass('show');
     });
+
+    // artist section의 view more 버튼 동작 애니메이션
+    
+    $('#artist .more').on('click', function() {
+        var wd = $(window).width();
+        if(wd > 0 && wd < 1280) {
+            $('#artist ul li').eq(7).nextAll('li').stop().animate({'height': 112, 'opacity': 1}, 500);
+        } else {
+            $('#artist ul li').eq(7).nextAll('li').stop().animate({'height': 244, 'opacity': 1}, 500);
+        }
+    });
 });
