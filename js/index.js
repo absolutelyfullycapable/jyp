962049af1925a136a4b7d44efb5c394b1b5befd8
absolutelyfullycapable <lovinsurfaces@gmail.com>
2020-11-15 19:42:53 +0900

    modify index, about files - mobile nav animation

--- js/index.js
+++ js/index.js
@@ -4,14 +4,16 @@ $(function() {
         e.preventDefault();
     });
 
-    // header 안 .mobile_btn 누르면 nav 나타남
+    // header 안 .mobile_btn 누르면 nav 나타남 + .nav_back 애니메이션
     $('header .mobile_btn').on('click', function() {
         $('nav').addClass('show');
+        $('.nav_back').addClass('show');
     });
 
-    // nav 안 X 버튼 누르면 nav 다시 사라짐
+    // nav 안 X 버튼 누르면 nav 다시 사라짐 + .nav_back 애니메이션
     $('nav div img').on('click', function() {
         $('nav').removeClass('show');
+        $('.nav_back').removeClass('show');
     });
 
     // .lang 부분 클릭 애니메이션
