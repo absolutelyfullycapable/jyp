b5b5e425dec88060521dbba4f10cc5f6ee1c106d
absolutelyfullycapable <lovinsurfaces@gmail.com>
2020-12-01 21:28:04 +0900

    modify index HTML, CSS, JS/jQuery - banner section

--- js/index.js
+++ js/index.js
@@ -21,4 +21,18 @@ $(function() {
         $(this).siblings().removeClass('on');
         $(this).addClass('on');
     });
+
+    // Swiper 플러그인
+    var swiper = new Swiper('.swiper-container', {
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction:false
+        },
+        loop: true,
+        speed: 2000,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: 'true'
+        }
+    });
 });
